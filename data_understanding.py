f404e7e3d1ee83af2e207be78193450600a2bdf7
dev-ezzy <bettezra07@gmail.com>
2023-11-28 19:38:27 +0300

    narudi sahi

--- data_understanding.py
+++ data_understanding.py
@@ -3,22 +3,45 @@ from IPython import display
 
 import pandas as pd
 
+import pandas as pd
+
 def data_summary(file_name):
     # Read the CSV file into a DataFrame
     file_path = f'./{file_name}'  # Assuming the file is in the same directory as the script
-    display.df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path)
 
     # Show the first 5 rows of the DataFrame
     print("First 5 rows of the DataFrame:")
-    display.df.head
+    print(df.head())  # Use df.head() to display the DataFrame
 
     # Display summary information about the DataFrame
     print("\nDataFrame Info:")
-    display.df.info()
+    print(df.info())  # Use print to display DataFrame info
 
     # Show summary statistics of the DataFrame
     print("\nSummary Statistics:")
-    display.df.describe()
+    print(df.describe())  # Use print to display DataFrame statistics
+
+# Call the function with the file name as an argument
+ # Replace 'your_file.csv' with the actual file name
+
+
+# def data_summary(file_name):
+#     # Read the CSV file into a DataFrame
+#     file_path = f'./{file_name}'  # Assuming the file is in the same directory as the script
+#     display.df = pd.read_csv(file_path)
+
+#     # Show the first 5 rows of the DataFrame
+#     print("First 5 rows of the DataFrame:")
+#     display.df.head
+
+#     # Display summary information about the DataFrame
+#     print("\nDataFrame Info:")
+#     display.df.info()
+
+#     # Show summary statistics of the DataFrame
+#     print("\nSummary Statistics:")
+#     display.df.describe()
 
 
 # print("""
